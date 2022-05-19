5f39f7b25234388e59527e2f43c493e28a7d3e05
Michael Grimsley <24793858+chillithechillinchinchilla@users.noreply.github.com>
2022-05-19 12:09:53 -0400

    Basic login with Level 1 security

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = requore("ejs");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -10,8 +11,69 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+//require mongoose, setup local connection (or atlas ect), setup schema, create model, register post ect/
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const User = mongoose.model("User", userSchema);
+
+// Create a new user from the register page.
+app.post("/register", function(req,res){
+  User.create({
+    email: req.body.username,
+    password: req.body.password
+  }, function(err){
+      if (err){
+        console.log(err);
+      }
+      else {
+        res.render("secrets");
+      }
+    } //end callback
+  ); // end create
+}); //end post
+
+app.post("/login", function(req,res){
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne( {email: username }, function(err, foundUser){
+      if(err){
+        console.log(err);
+      } else if(foundUser){
+            if (foundUser.password === password){
+              console.log("Creditials Match");
+              res.render("secrets");
+            }else{
+              console.log("No password match.");
+              res.render("login");
+            }
+        } else {
+          console.log("No matching users found");
+          res.render("login");
+        }
+    });//end find
+
+}); //end post
 
 
-app.listen(3000, function({
+app.get("/", function(req,res){
+    res.render("home");
+});
+
+app.get("/login", function(req,res){
+    res.render("login");
+});
+
+app.get("/register", function(req,res){
+  res.render("register");
+});
+
+
+app.listen(3000, function(){
   console.log("Server Started on port 3000");
-}))
+});
